87a1e2ea0cd2da159dc2743ecf15ad3f03393dd9
santoshPagire <santoshpagire1007@gmail.com>
2024-09-16 10:24:56 +0530

    App code added

---
+++ server.js
@@ -0,0 +1,15 @@
+const express = require('express')
+
+const app = express()
+
+
+
+app.get('/', (req, res) => {
+    res.json({
+        message: "Hello World "
+    })
+})
+
+app.listen(8080, () => {
+    console.log('server running on port 8080')
+})
